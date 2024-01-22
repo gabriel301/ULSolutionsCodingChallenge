5ec2d6c9e469791eebb6b8601547dc8a18d5747b
Gabriel <gabriel301@gmail.com>
2024-01-21 23:19:46 -0300

    Finish Expression validations and validations unit testing

---
+++ ExpressionEvaluatorAPI/Domain/Entities/StringExpression.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Exceptions;
+using Shared.RegexPatterns;
+using Shared.ReShared.Resources.Validationsoures.Validation;
+using System.Text.RegularExpressions;
+
+namespace Domain.Entities;
+
+public sealed class StringExpression
+{
+
+    public string ExpresionString { get; private set; }
+
+    public StringExpression(string expressionString)
+    {
+        Validate(expressionString);
+        ExpresionString = expressionString;
+    }
+
+    private void Validate(string expressionString)
+    {
+        if (expressionString == null || expressionString.Trim().Equals(string.Empty))
+        {
+            throw new DomainException(ValidationResources.ResourceManager.GetString("Null_Or_Empty_String"));
+        }
+
+        if (Regex.Match(expressionString, ValidationPatterns.INVALID_CHARACTERS).Success)
+        {
+            throw new DomainException(ValidationResources.ResourceManager.GetString("Invalid_Characters"));
+        }
+
+        if (Regex.Match(expressionString, ValidationPatterns.CONSECUTIVE_OPERATORS).Success)
+        {
+            throw new DomainException(ValidationResources.ResourceManager.GetString("Consecutive_Operators"));
+        }
+
+        if (Regex.Match(expressionString, ValidationPatterns.STARTS_OR_ENDS_WITH_NON_DIGITS).Success)
+        {
+            throw new DomainException(ValidationResources.ResourceManager.GetString("Starts_Or_Ends_With_Operator"));
+        }
+
+        if (Regex.Match(expressionString, ValidationPatterns.CONTAINS_ONLY_DIGITS).Success)
+        {
+            throw new DomainException(ValidationResources.ResourceManager.GetString("Contains_Only_Digits"));
+        }
+    }
+}
+
---
+++ ExpressionEvaluatorAPI/UnitTests/Domain/ExpressionTest.cs
@@ -0,0 +1,129 @@
+﻿using Domain.Entities;
+using Domain.Exceptions;
+using Shared.ReShared.Resources.Validationsoures.Validation;
+
+namespace UnitTests.Domain;
+
+public class ExpressionTest
+{
+
+    [Fact(DisplayName = nameof(Instantiate))]
+    [Trait("Domain", "StringExpression")]
+    public void Instantiate()
+    {
+        string data = "1+1+1";
+
+        StringExpression expression = new StringExpression(data);
+
+        Assert.NotNull(expression);
+        Assert.Equal(data, expression.ExpresionString);
+    }
+
+
+    [Theory(DisplayName = nameof(IsNullOrEmptyString))]
+    [Trait("Domain", "StringExpression")]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("   ")] //Spaces
+    [InlineData("       ")] //Tabs
+    public void IsNullOrEmptyString(string? data)
+    {
+        Action action = () => { new StringExpression(data); };
+
+        var exception = Assert.Throws<DomainException>(action);
+
+        Assert.NotNull(exception);
+        Assert.IsType(typeof(DomainException), exception);
+        Assert.Equal(ValidationResources.ResourceManager.GetString("Null_Or_Empty_String"), exception.Message);
+
+    }
+
+
+    [Theory(DisplayName = nameof(ContainsInvalidCharacters))]
+    [Trait("Domain", "StringExpression")]
+    [InlineData("A+1+3")]
+    [InlineData("1+2=3")]
+    [InlineData("1.1+2+3")]
+    [InlineData("1 + 1")] //Spaces
+    [InlineData("1  +1")] //Tabs
+    [InlineData("1+1!2")] //Tabs
+    public void ContainsInvalidCharacters(string? data)
+    {
+        Action action = () => { new StringExpression(data); };
+
+        var exception = Assert.Throws<DomainException>(action);
+
+        Assert.NotNull(exception);
+        Assert.IsType(typeof(DomainException), exception);
+        Assert.Equal(ValidationResources.ResourceManager.GetString("Invalid_Characters"), exception.Message);
+
+    }
+
+
+    [Theory(DisplayName = nameof(ContainsSequentialOperators))]
+    [Trait("Domain", "StringExpression")]
+    [InlineData("++1+2")]
+    [InlineData("--1+2")]
+    [InlineData("//1+1")]
+    [InlineData("**1+2")]
+    [InlineData("1+-2")]
+    [InlineData("1-*2")]
+    [InlineData("1-*+/-2")]
+    [InlineData("1-2+-")]
+    public void ContainsSequentialOperators(string? data)
+    {
+        Action action = () => { new StringExpression(data); };
+
+        var exception = Assert.Throws<DomainException>(action);
+
+        Assert.NotNull(exception);
+        Assert.IsType(typeof(DomainException), exception);
+        Assert.Equal(ValidationResources.ResourceManager.GetString("Consecutive_Operators"), exception.Message);
+
+    }
+
+
+    [Theory(DisplayName = nameof(StartsOrEndsWithOperators))]
+    [Trait("Domain", "StringExpression")]
+    [InlineData("+1+2")]
+    [InlineData("-1+2-")]
+    [InlineData("/1+1")]
+    [InlineData("*1+2")]
+    [InlineData("1+2-")]
+    [InlineData("1+2*")]
+    [InlineData("1-2/")]
+    [InlineData("+1-2-")]
+    public void StartsOrEndsWithOperators(string? data)
+    {
+        Action action = () => { new StringExpression(data); };
+
+        var exception = Assert.Throws<DomainException>(action);
+
+        Assert.NotNull(exception);
+        Assert.IsType(typeof(DomainException), exception);
+        Assert.Equal(ValidationResources.ResourceManager.GetString("Starts_Or_Ends_With_Operator"), exception.Message);
+
+    }
+
+
+    [Theory(DisplayName = nameof(ContainsOnlyDigits))]
+    [Trait("Domain", "StringExpression")]
+    [InlineData("0")]
+    [InlineData("1")]
+    [InlineData("123")]
+    [InlineData("12345")]
+    public void ContainsOnlyDigits(string? data)
+    {
+        Action action = () => { new StringExpression(data); };
+
+        var exception = Assert.Throws<DomainException>(action);
+
+        Assert.NotNull(exception);
+        Assert.IsType(typeof(DomainException), exception);
+        Assert.Equal(ValidationResources.ResourceManager.GetString("Contains_Only_Digits"), exception.Message);
+
+    }
+
+
+}
+
